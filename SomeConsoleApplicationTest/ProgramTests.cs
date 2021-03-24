69562d9307f44a36c93d7daa112b4067f3f0e5ef
Joji <jojify@gmail.com>
2021-03-24 16:23:51 +0530

    deleted a test

--- SomeConsoleApplicationTest/ProgramTests.cs
+++ SomeConsoleApplicationTest/ProgramTests.cs
@@ -17,17 +17,5 @@ namespace SomeConsoleApplicationTest
             Assert.IsTrue(actual);
         }
 
-        [TestMethod]
-        public void Passthrough_ReturnsTheGivenValue()
-        {
-            // Arrange
-            var value = 42;
-
-            // Act
-            var actual = Program.Passthrough(value);
-
-            // Assert
-            Assert.AreEqual(actual, value);
-        }
     }
 }
